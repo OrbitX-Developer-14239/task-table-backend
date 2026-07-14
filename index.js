028df4bb4f4d8e6def66b5312ceb332c92fc7022
Mirvohid2412 <mirvohidmirzohidov25@gmail.com>
2026-07-14 17:33:17 +0500

    cors fixed

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: function (origin, callback) {
+        callback(null, true);
+    },
     credentials: true,
 }));
 
